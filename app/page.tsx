9bbc2f4d65225929520ecfc9320b398e6911f231
vijaybhaskarchukkala <vijaybhaskarchukkala045@gmail.com>
2026-09-04 20:13:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { AccessGate } from "@/components/campus/access-gate"
 import { SessionProvider } from "@/components/campus/session-provider"
 import { AuthGate } from "@/components/campus/auth-gate"
+import { PushNotificationInit } from "@/components/campus/push-notification-init"
 
 export default function Page() {
   return (
     <AccessGate>
       <SessionProvider>
+        <PushNotificationInit />
         <AuthGate />
       </SessionProvider>
     </AccessGate>
